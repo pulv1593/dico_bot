fd07f4803f926adf7df11bf49a2db1f1398e08bd
Jiho Jeong <wldwlgh95@gmail.com>
2025-01-14 17:20:44 +0900

    불필요한 try문 제거

--- bot.py
+++ bot.py
@@ -25,7 +25,6 @@ bot = commands.Bot(
 # 서버별 대기열 저장
 queues = defaultdict(deque)
 
-
 #봇 이벤트 처리 함수 모음 
 @bot.event
 async def on_ready():
@@ -35,27 +34,23 @@ async def on_ready():
 # 노래봇을 음성채팅에 데려오는 명령어
 @bot.command(aliases=['입장'])
 async def join(ctx):
-    try:
-        embed = discord.Embed(title = "디스코드 봇 도우미(개발용)", description = "음성 채널 개발용 디스코드 봇",color=0x00ff56)
-        channel = ctx.author.voice.channel
-        
-        if ctx.author.voice is None:
-            embed.add_field(name=':exclamation:', value="음성 채널에 유저가 존재하지 않습니다. 1명 이상 입장해주세요.")
-            await ctx.send(embed=embed)
-            raise commands.CommandInvokeError("사용자가 존재하는 음성 채널을 찾지 못했습니다.")
-        
-        if ctx.voice_client is not None:
-            embed.add_field(name=":robot:",value="사용자가 있는 채널로 이동합니다.", inline=False)
-            await ctx.send(embed=embed)
-            print("음성 채널 정보: {0.author.voice}".format(ctx))
-            print("음성 채널 이름: {0.author.voice.channel}".format(ctx))
-            return await ctx.voice_client.move_to(channel)
+    embed = discord.Embed(title = "디스코드 봇 도우미(개발용)", description = "음성 채널 개발용 디스코드 봇",color=0x00ff56)
+    channel = ctx.author.voice.channel
+    
+    if ctx.author.voice is None:
+        embed.add_field(name=':exclamation:', value="음성 채널에 유저가 존재하지 않습니다. 1명 이상 입장해주세요.")
+        await ctx.send(embed=embed)
+        raise commands.CommandInvokeError("사용자가 존재하는 음성 채널을 찾지 못했습니다.")
+    
+    if ctx.voice_client is not None:
+        embed.add_field(name=":robot:",value="사용자가 있는 채널로 이동합니다.", inline=False)
+        await ctx.send(embed=embed)
+        print("음성 채널 정보: {0.author.voice}".format(ctx))
+        print("음성 채널 이름: {0.author.voice.channel}".format(ctx))
+        return await ctx.voice_client.move_to(channel)
             
-        await channel.connect()
-    except Exception as e:
-        await ctx.send(f"에러 발생: {e}")
-    finally:
-        pass
+    await channel.connect()
+
 #노래봇을 음성채널에서 내보내는 명령어 
 @bot.command(aliases=['나가기'])
 async def out(ctx):
@@ -74,8 +69,6 @@ async def out(ctx):
         embed = discord.Embed(color=0xf66c24)
         embed.add_field(name=":x:",value="봇이 존재하는 채널을 찾는 데 실패했습니다.")
         await ctx.send(embed=embed)
-    finally:
-        pass
 
 # /play 명령어 내장 함수 모음
 # 1. URL 유효성 검사 함수
@@ -140,193 +133,172 @@ async def play_next_song(ctx):
 # 노래 재생 명령어
 @bot.command(aliases=['재생'])
 async def p(ctx, *, search: str):    
-    try:
-        #활성화된 음성채널에 없을 시
-        if ctx.author.voice is None:
-            await ctx.send("음성 채널에 입장한 후 명령어를 사용해주세요.")
-            return
-        #노래봇이 음성채널에 들어가있지 않은 경우
-        if ctx.voice_client is None:
-            await ctx.invoke(bot.get_command('join'))
+    #활성화된 음성채널에 없을 시
+    if ctx.author.voice is None:
+        await ctx.send("음성 채널에 입장한 후 명령어를 사용해주세요.")
+        return
+    #노래봇이 음성채널에 들어가있지 않은 경우
+    if ctx.voice_client is None:
+        await ctx.invoke(bot.get_command('join'))
 
-        #url로 입력한 경우 처리
-        if is_valid_url(search):
-            try:
-                ytdl_opts = {
-                'format': 'bestaudio/best',
-                'quiet': True,
-                }
-                ytdl = YoutubeDL(ytdl_opts)
-                info = ytdl.extract_info(search, download=False)
-                url = info.get('url')
-                title = info.get('title', '알 수 없는 제목')
-
-                # 대기열에 추가
-                add_to_queue(ctx, {'url': url, 'title': title})
-        
-                # 대기열에 추가된 곡 표시
-                embed = discord.Embed(title=":musical_note: 대기열 추가", description=f"**{title}**", color=0x1DB954)
-                await ctx.send(embed=embed)
-
-                # 노래 재생
-                if not ctx.voice_client.is_playing():
-                    await play_next_song(ctx)
-                else:
-                    await ctx.send("노래가 이미 재생 중입니다. 대기열에 추가되었습니다.")
-                    return
-            except Exception as e:
-                await ctx.send("URL에서 음악을 가져올 수 없습니다. 링크를 확인해주세요.")
-                print(f"에러 발생: {e}")
-            finally:
-                return
-        # 키워드 검색인 경우
-        # "검색 중입니다!" 메시지 보내기
-        loading_message = await ctx.send("🔍 노래를 찾고있어요..! 조금만 기다려주세요...")
+    #url로 입력한 경우 처리
+    if is_valid_url(search, False):
         try:
-            # 노래 검색
-            entries = await search_song(ctx, search)
-            #검색 결과가 없는 경우
-            if not entries:
-                await loading_message.edit(content="검색 결과가 없습니다. 다른 키워드로 시도해주세요.")
-                return
+            ytdl_opts = {
+            'format': 'bestaudio/best',
+            'quiet': True,
+            }
+            ytdl = YoutubeDL(ytdl_opts)
+            info = ytdl.extract_info(search, download=False)
+            url = info.get('url')
+            title = info.get('title', '알 수 없는 제목')
 
-            # 검색결과를 discord 메세지로 출력
-            embed = discord.Embed(title="검색 결과", description="원하는 곡 번호를 입력해주세요!\n '0'을 입력하면 취소됩니다.", color=0x1DB954)
-            for i, entry in enumerate(entries[:3]):  # 최대 3개 출력
-                embed.add_field(
-                    name=f"{i+1}. {entry['title']}", 
-                    value="\u200b",  # 빈 줄 표시
-                    inline=False
-                )
-            # 기존 메시지 수정
-            await loading_message.edit(content=None, embed=embed)
+            # 대기열에 추가
+            add_to_queue(ctx, {'url': url, 'title': title})
+    
+            # 대기열에 추가된 곡 표시
+            embed = discord.Embed(title=":musical_note: 대기열 추가", description=f"**{title}**", color=0x1DB954)
+            await ctx.send(embed=embed)
 
-            # 사용자 입력 대기
-            def check(msg):
-                return msg.author == ctx.author and msg.content.isdigit() and 0 <= int(msg.content) <= len(entries[:3])
-        
-            #60초간 사용자 입력 대기
-            try:
-                msg = await bot.wait_for('message', check=check, timeout=60.0)
-                selection = int(msg.content)
-
-                if selection == 0:
-                    await ctx.send("노래 검색이 취소되었습니다.")
-                    return
-            
-                selected_entry = entries[selection - 1]
-
-                # 선택된 음원 재생
-                url = selected_entry.get('url') or selected_entry.get('webpage_url')
-                title = selected_entry.get('title', '알 수 없는 제목')
-
-                # 대기열에 추가
-                add_to_queue(ctx, {'url': url, 'title': title})
-
-                embed = discord.Embed(title=":musical_note: 대기열에 추가되었습니다!", description=f"**{title}**", color=0x1DB954)
-                await ctx.send(embed=embed)
-
-                # 오디오 재생
-                if not ctx.voice_client.is_playing():
-                    await play_next_song(ctx)
-                else:
-                    # 현재 대기열 출력
-                    embed = discord.Embed(title="현재 대기열", color=0x1DB954)
-                    for i, song in enumerate(queues[ctx.guild.id]):
-                        embed.add_field(name=f"{i+1}. {song['title']}", value="\u200b", inline=False)
-                    await ctx.send(embed=embed)  
-            except asyncio.TimeoutError:
-                await ctx.send("시간 초과로 명령어가 취소되었습니다.")
+            # 노래 재생
+            if not ctx.voice_client.is_playing():
+                await play_next_song(ctx)
+            else:
+                await ctx.send("노래가 이미 재생 중입니다. 대기열에 추가되었습니다.")
                 return
         except Exception as e:
-            await ctx.send("검색을 실패했습니다.")
+            await ctx.send("URL에서 음악을 가져올 수 없습니다. 링크를 확인해주세요.")
             print(f"에러 발생: {e}")
+        finally:
+            return
+    # 키워드 검색인 경우
+    # "검색 중입니다!" 메시지 보내기
+    loading_message = await ctx.send("🔍 노래를 찾고있어요..! 조금만 기다려주세요...")
+    try:
+        # 노래 검색
+        entries = await search_song(ctx, search)
+        #검색 결과가 없는 경우
+        if not entries:
+            await loading_message.edit(content="검색 결과가 없습니다. 다른 키워드로 시도해주세요.")
+            return
+
+        # 검색결과를 discord 메세지로 출력
+        embed = discord.Embed(title="검색 결과", description="원하는 곡 번호를 입력해주세요!\n '0'을 입력하면 취소됩니다.", color=0x1DB954)
+        for i, entry in enumerate(entries[:3]):  # 최대 3개 출력
+            embed.add_field(
+                name=f"{i+1}. {entry['title']}", 
+                value="\u200b",  # 빈 줄 표시
+                inline=False
+            )
+        # 기존 메시지 수정
+        await loading_message.edit(content=None, embed=embed)
+
+        # 사용자 입력 대기
+        def check(msg):
+            return msg.author == ctx.author and msg.content.isdigit() and 0 <= int(msg.content) <= len(entries[:3])
+    
+        #60초간 사용자 입력 대기
+        try:
+            msg = await bot.wait_for('message', check=check, timeout=60.0)
+            selection = int(msg.content)
+
+            if selection == 0:
+                await ctx.send("노래 검색이 취소되었습니다.")
+                return
+        
+            selected_entry = entries[selection - 1]
+
+            # 선택된 음원 재생
+            url = selected_entry.get('url') or selected_entry.get('webpage_url')
+            title = selected_entry.get('title', '알 수 없는 제목')
+
+            # 대기열에 추가
+            add_to_queue(ctx, {'url': url, 'title': title})
+
+            embed = discord.Embed(title=":musical_note: 대기열에 추가되었습니다!", description=f"**{title}**", color=0x1DB954)
+            await ctx.send(embed=embed)
+
+            # 오디오 재생
+            if not ctx.voice_client.is_playing():
+                await play_next_song(ctx)
+            else:
+                # 현재 대기열 출력
+                embed = discord.Embed(title="현재 대기열", color=0x1DB954)
+                for i, song in enumerate(queues[ctx.guild.id]):
+                    embed.add_field(name=f"{i+1}. {song['title']}", value="\u200b", inline=False)
+                await ctx.send(embed=embed)  
+        except asyncio.TimeoutError:
+            await ctx.send("시간 초과로 명령어가 취소되었습니다.")
+            return
     except Exception as e:
-            await ctx.send("음악을 재생할 수 없습니다. 링크 또는 키워드를 확인해주세요.")
-            print(f"에러 발생: {e}")
-    finally:
-        pass
+        await ctx.send("검색을 실패했습니다.")
+        print(f"에러 발생: {e}")
 
 #노래 재생을 끝내고 내보내기
 @bot.command(aliases=['멈춤', '정지'])
 async def stop(ctx):
-    try:
-        if ctx.voice_client is None:
-            embed = discord.Embed(color=0xf66c24)
-            embed.add_field(name=":x:", value="봇이 음성 채널에 연결되어 있지 않습니다.")
-            await ctx.send(embed=embed)
-            return
-        else:
-            # 음악이 재생 중이면 멈추기
-            if ctx.voice_client.is_playing():
-                ctx.voice_client.stop()
-            # 음성 채널에서 봇을 나가게 함
-            await ctx.voice_client.disconnect()
-            
-            embed = discord.Embed(color=0x00ff56)
-            embed.add_field(name=":stop_button:", value="음악을 멈추고 음성 채널에서 나갔습니다.", inline=False)
-            await ctx.send(embed=embed)
-            return
-    except Exception as e:
-        await ctx.send(f"에러 발생: {e}")
-    finally:
-        pass
-
+    if ctx.voice_client is None:
+        embed = discord.Embed(color=0xf66c24)
+        embed.add_field(name=":x:", value="봇이 음성 채널에 연결되어 있지 않습니다.")
+        await ctx.send(embed=embed)
+        return
+    else:
+        # 음악이 재생 중이면 멈추기
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+        # 음성 채널에서 봇을 나가게 함
+        await ctx.voice_client.disconnect()
+        
+        embed = discord.Embed(color=0x00ff56)
+        embed.add_field(name=":stop_button:", value="음악을 멈추고 음성 채널에서 나갔습니다.", inline=False)
+        await ctx.send(embed=embed)
+        return
 
 # 대기열 확인 명령어
 @bot.command(aliases=['대기열'])
 async def queue(ctx):
-    try:
-        if ctx.guild.id not in queues or len(queues[ctx.guild.id]) == 0:
-            await ctx.send("현재 대기열에 노래가 없습니다.")
-            return
-        # 대기열이 있을 경우 출력
-        embed = discord.Embed(title="현재 대기열", color=0x1DB954)
-        for i, song in enumerate(queues[ctx.guild.id]):
-            embed.add_field(name=f"{i+1}. {song['title']}", value="\u200b", inline=False)
+    if ctx.guild.id not in queues or len(queues[ctx.guild.id]) == 0:
+        await ctx.send("현재 대기열에 노래가 없습니다.")
+        return
+    # 대기열이 있을 경우 출력
+    embed = discord.Embed(title="현재 대기열", color=0x1DB954)
+    for i, song in enumerate(queues[ctx.guild.id]):
+        embed.add_field(name=f"{i+1}. {song['title']}", value="\u200b", inline=False)
 
-        await ctx.send(embed=embed)
-    except Exception as e:
-        await ctx.send(f"에러 발생: {e}")
-    finally:
-        pass
+    await ctx.send(embed=embed)
+
 # 재생중인 노래 스킵 명령어
 @bot.command(aliases=['스킵'])
 async def skip(ctx):
-    try:
-        if ctx.voice_client is None:
-            embed = discord.Embed(color=0xf66c24)
-            embed.add_field(name=":x:", value="봇이 음성 채널에 연결되어 있지 않습니다.")
-            await ctx.send(embed=embed)
-            return
-
-        if not ctx.voice_client.is_playing():
-            embed = discord.Embed(color=0xf66c24)
-            embed.add_field(name=":x:", value="현재 재생 중인 음악이 없습니다.")
-            await ctx.send(embed=embed)
-            return
-        
-        # 대기열에 다음 곡이 있는지 확인
-        if len(queues[ctx.guild.id]) == 0:
-            embed = discord.Embed(color=0xf66c24)
-            embed.add_field(name=":x:", value="대기열에 다음 곡이 없습니다. stop 명령어를 이용해주세요.")
-            await ctx.send(embed=embed)
-            return
-
-        # 현재 곡을 멈추고 다음 곡 재생
-        ctx.voice_client.stop()
-        embed = discord.Embed(color=0x1DB954)
-        embed.add_field(name=":fast_forward:", value="현재 곡을 스킵하고 다음 곡을 재생합니다.", inline=False)
+    if ctx.voice_client is None:
+        embed = discord.Embed(color=0xf66c24)
+        embed.add_field(name=":x:", value="봇이 음성 채널에 연결되어 있지 않습니다.")
         await ctx.send(embed=embed)
+        return
 
-        # 약간의 지연 후 다음 곡 재생
-        await asyncio.sleep(1)  # 1초 지연을 추가
-        if not ctx.voice_client.is_playing():  # 중복 재생 방지
-            await play_next_song(ctx)
-    except Exception as e:
-        await ctx.send(f"에러 발생: {e}")
-    finally:
-        pass
+    if not ctx.voice_client.is_playing():
+        embed = discord.Embed(color=0xf66c24)
+        embed.add_field(name=":x:", value="현재 재생 중인 음악이 없습니다.")
+        await ctx.send(embed=embed)
+        return
+    
+    # 대기열에 다음 곡이 있는지 확인
+    if len(queues[ctx.guild.id]) == 0:
+        embed = discord.Embed(color=0xf66c24)
+        embed.add_field(name=":x:", value="대기열에 다음 곡이 없습니다. stop 명령어를 이용해주세요.")
+        await ctx.send(embed=embed)
+        return
+
+    # 현재 곡을 멈추고 다음 곡 재생
+    ctx.voice_client.stop()
+    embed = discord.Embed(color=0x1DB954)
+    embed.add_field(name=":fast_forward:", value="현재 곡을 스킵하고 다음 곡을 재생합니다.", inline=False)
+    await ctx.send(embed=embed)
+
+    # 약간의 지연 후 다음 곡 재생
+    await asyncio.sleep(1)  # 1초 지연을 추가
+    if not ctx.voice_client.is_playing():  # 중복 재생 방지
+        await play_next_song(ctx)
     
 @bot.command(aliases=['다시재생'])
 async def rep(ctx):
